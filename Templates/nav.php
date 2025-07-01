6f4adc3b31d32c53c99396f39b08473baeb1518f
Edwin Kimuruae <sururu.kimuruae@kimuruae.edu>
2025-07-01 23:23:22 +0300

    New updates!!!

---
+++ Templates/nav.php
@@ -0,0 +1,33 @@
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">
+                <img src="images/logo.png" alt="Student Welfare Logo" width="30" height="30" class="d-inline-block align-top me-2">
+                Student Welfare
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                <span class="visually-hidden">Toggle navigation</span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.html"><i class="fas fa-home me-1"></i> Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="booking.html"><i class="fas fa-bed me-1"></i> Hostels</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="emergency.html"><i class="fas fa-ambulance me-1"></i> Emergency</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="counselling.html"><i class="fas fa-comments me-1"></i> Counseling</a>
+                    </li>
+                </ul>
+                <div class="d-flex">
+                    <a href="login.html" class="btn btn-outline-light me-2"><i class="fas fa-sign-in-alt me-1"></i> Login</a>
+                    <a href="register.html" class="btn btn-light"><i class="fas fa-user-plus me-1"></i> Register</a>
+                </div>
+            </div>
+        </div>
+    </nav>
