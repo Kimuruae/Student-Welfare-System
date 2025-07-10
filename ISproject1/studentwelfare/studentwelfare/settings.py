81c163064c2d67c1178aa98dc9f9ec1603dfc146
Edwin Kimuruae <sururu.kimuruae@kimuruae.edu>
2025-07-10 19:05:55 +0300

    New updates

--- ISproject1/studentwelfare/studentwelfare/settings.py
+++ ISproject1/studentwelfare/studentwelfare/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
